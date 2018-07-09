0fcee0bff78a4793806fff70f31f5b2d4feea296
Anson Liu <ansonl@users.noreply.github.com>
2018-07-08 22:00:23 -0400

    initial

---
+++ js/featureManagement.js
@@ -0,0 +1,150 @@
+function loadItems() {
+
+	var progress = 0;
+	var total = 0;
+
+	$('#statusAlert').show();
+	$('#statusProgressBar>div').css('width', progress+'%');
+
+	for (var stateAbbr in statesDict) {
+		total++;
+	}
+
+	for (var stateAbbr in statesDict) {
+		$.ajax({
+			dataType: "json",
+			url: 'hikes/' + statesDict[stateAbbr].toLowerCase().split(' ').join('-') + '.geojson',
+			data: null,
+			success: function(data, textStatus, jqXHR) {
+				addToMap(data['features'])
+			},
+			error: function(data, textStatus, jqXHR) {
+				//404 means state does not have geojson file for it yet
+				//non-404 indicates another error
+				if (data.status != 404) {
+					console.log(data);
+					$('#statusAlertText').text('Loading ' + statesDict[stateAbbr] + ' error: ' + data.status + textStatus);
+				}
+			},
+			complete(jqXHR, textStatus) {
+				progress++
+				$('#statusProgressBar>div').css('width', (progress/total)*100+'%');
+
+				if (progress == total)
+					$('#statusAlert').hide();
+
+			}
+		});
+	}
+}
+
+function addToMap(featureCollection) {
+	hikesLayer.addData(featureCollection)
+}
+
+//Create marker object with correct look for each geoJsonPoint
+function pointToLayer(geoJsonPoint, latlng) {
+	console.log(geoJsonPoint)
+
+	var createdMarker = L.marker(latlng); 
+
+	if (geoJsonPoint.properties && geoJsonPoint.properties.type) {
+		if (geoJsonPoint.properties.type == 'start') {
+			var customIcon = L.divIcon({className: 'trailhead-icon', iconSize: new L.Point(6, 20)})
+			createdMarker = L.marker(latlng, {icon: customIcon});
+		}
+
+		if (geoJsonPoint.properties.type == 'peak') {
+			var peakClass = 'peak-icon';
+			var difficulty = 0; //default difficulty, use .peak-icon
+			//Use .peak-icon-X, where X = difficulty [1,5]
+			if (geoJsonPoint.properties.difficulty && geoJsonPoint.properties.difficulty > 0 && geoJsonPoint.properties.difficulty <= 5) {
+				difficulty = geoJsonPoint.properties.difficulty;
+				peakClass = peakClass + ' peak-icon' + (difficulty > 0 ? '-'+difficulty : '')
+			}
+			var customIcon = L.divIcon({className: peakClass, iconSize: new L.Point(30, 14)})
+			createdMarker = L.marker(latlng, {icon: customIcon});
+		}
+
+		if (geoJsonPoint.properties.type == 'canyon') {
+			var peakClass = 'canyon-icon';
+			var difficulty = 0; //default difficulty, use .peak-icon
+			//Use .peak-icon-X, where X = difficulty [1,5]
+			if (geoJsonPoint.properties.difficulty && geoJsonPoint.properties.difficulty > 0 && geoJsonPoint.properties.difficulty <= 5) {
+				difficulty = geoJsonPoint.properties.difficulty;
+				peakClass = peakClass + ' canyon-icon' + (difficulty > 0 ? '-'+difficulty : '')
+			}
+			var customIcon = L.divIcon({className: peakClass, iconSize: new L.Point(25, 20)})
+			createdMarker = L.marker(latlng, {icon: customIcon});
+		}
+	}
+
+	var visible = true;
+	createdMarker.on('click', function () {
+		if (visible) {
+			createdMarker.setOpacity(0.5);
+		} else {
+			createdMarker.setOpacity(1);
+		}
+		visible = !visible;
+	});
+
+	//Set tooltip with name
+	if (geoJsonPoint.properties && geoJsonPoint.properties.name) {
+		var tooltipText = geoJsonPoint.properties.name;
+
+		if (geoJsonPoint.properties.type && geoJsonPoint.properties.type == 'start') {
+			tooltipText = 'START' + (tooltipText.length > 0 ? ' - ' + tooltipText : '');
+		}
+
+		createdMarker.bindTooltip(tooltipText, {
+			sticky: true,
+			direction: 'bottom'
+		})
+	}
+
+	//Set popup with popupContent text
+	var popupText = '';
+	if (geoJsonPoint.properties && geoJsonPoint.properties.popupContent) {
+		popupText = geoJsonPoint.properties.popupContent
+	}
+
+	if (geoJsonPoint.properties && geoJsonPoint.properties.involves && geoJsonPoint.properties.involves.length > 0) {
+		popupText += (popupText.length > 0 ? '</br>' : '') + '<strong>Involves:</strong><ul>';
+		for (var i = 0; i < geoJsonPoint.properties.involves.length; i++) {
+			popupText += '<li>' + geoJsonPoint.properties.involves[i] + '</li>'
+		}
+		popupText += '</ul>'
+	}
+
+	if (popupText.length > 0) {
+		createdMarker.bindPopup(popupText);
+	}
+
+	return createdMarker;
+	
+}
+
+//Create correct tooltip for subpoint
+function onEachFeatureSubPoint(feature, layer) {
+	
+}
+
+function onEachFeatureMainPoint(feature, layer) {
+		//Create subPoint layer for subfeatures
+		//Show subPoint layer when parent marker selected
+		if (feature.properties && feature.properties.subFeatures) {
+			var subPointsLayer = L.geoJSON(false, {
+				pointToLayer: pointToLayer,
+				onEachFeature: onEachFeatureSubPoint
+			})
+			subPointsLayer.addData(feature.properties.subFeatures)
+
+			layer.on('click', function() {
+				if (visibleStartLayer)
+					hhMap.removeLayer(visibleStartLayer);
+				visibleStartLayer = subPointsLayer
+				hhMap.addLayer(visibleStartLayer);
+			})
+		}
+	}
---
+++ js/main.js
@@ -0,0 +1,142 @@
+$( document ).ready(pageReady);
+
+var hhMap;
+var hikesLayer;
+var visibleStartLayer;
+var saveControl;
+
+function pageReady() {
+	console.log("page ready");
+
+	hhMap = L.map('mapid')
+
+	//Background tile layer to show loading
+	//https://github.com/Leaflet/Leaflet/issues/4866#issuecomment-243485280
+	var backgroundTileLayer = L.tileLayer.offline("css/images/repeating-grid.jpg");
+	hhMap.addLayer(backgroundTileLayer);
+
+	var osmURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	var osmAttr = 'Map data from OSM';
+	var osmLayer = L.tileLayer.offline(osmURL, {
+		attribution: osmAttr
+	});
+
+	var otmURL = 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png';
+	var otmAttr = 'Map data from TopoMap';
+	var otmLayer = L.tileLayer.offline(otmURL, {
+		maxZoom: 14,
+		attribution: otmAttr
+	});
+
+	var u4mURL = 'https://tileserver.4umaps.com/{z}/{x}/{y}.png';
+	var u4mAttr = 'Map data from 4UMaps';
+	var u4mLayer = L.tileLayer.offline(u4mURL, {
+		attribution: u4mAttr
+	});
+
+	var hbmURL = 'http://{s}.tiles.wmflabs.org/hikebike/{z}/{x}/{y}.png';
+	var hbmAttr = 'Map data from 4UMaps';
+	var hbmLayer = L.tileLayer.offline(hbmURL, {
+		attribution: hbmAttr,
+		errorTileUrl: 'data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=='
+	});
+
+	var ewiURL = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}';
+	var ewiAttr = 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community';
+	var ewiLayer = L.tileLayer.offline(ewiURL, {
+		attribution: ewiAttr,
+		errorTileUrl: 'data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=='
+	});
+	
+	var baseMaps = {
+		"OpenStreetMap": osmLayer,
+		"OpenTopoMap": otmLayer,
+		"4UMaps": u4mLayer,
+		'Hike & Bike': hbmLayer,
+		'Satellite' : ewiLayer
+	}
+	
+
+	hhMap.addLayer(osmLayer);
+	
+	var layersControl = L.control.layers(baseMaps);
+	hhMap.addControl(layersControl);
+
+	var scaleControl = L.control.scale()
+	hhMap.addControl(scaleControl)
+
+	//Support offline tiles for OSM tiles
+	hhMap.on('baselayerchange', function(e) {
+		if (e.layer !== osmLayer)
+			hhMap.removeControl(saveControl);
+		else
+			saveControl = addSaveControls(e.layer);
+	});
+	
+	var locateControl = L.control.locate({
+		keepCurrentZoomLevel: true,
+		returnToPrevBounds: true,
+		enableHighAccuracy: true
+	});
+	hhMap.addControl(locateControl);
+
+	hhMap.setView([37.0902, -95.7129], 4);
+
+	//Create hikes marker layer
+	hikesLayer = L.geoJSON(false, {
+		pointToLayer: pointToLayer,
+		onEachFeature: onEachFeatureMainPoint
+	});
+	hhMap.addLayer(hikesLayer);
+
+	//Load hikes
+	loadItems();
+}
+
+function addSaveControls(baseLayer) {
+	//add buttons to save tiles in area viewed
+	var control = L.control.savetiles(baseLayer, {
+    //'zoomlevels': [13,16], //optional zoomlevels to save, default current zoomlevel
+    'confirm': function(layer, successcallback) {
+    	successcallback();
+    	document.getElementById('save-control').disabled = true;
+    	document.getElementById('save-control').className = "fa fa-circle-o-notch fa-spin";
+    },
+    'confirmRemoval': function(layer, successCallback) {
+    	successCallback();
+    },
+    'saveText': '<i class="fa fa-download" id="save-control" aria-hidden="true" title="Save tiles offline"></i><div id="storage-progress" title="Tiles saved"></div>',
+    'rmText': '<i class="fa fa-trash" id="remove-control" aria-hidden="true"  title="Remove saved tiles"></i>'
+  });
+	control.addTo(hhMap);
+	var previousStorageSize;
+	baseLayer.on('storagesize', function(e) {
+	previousStorageSize = e.storagesize
+	document.getElementById('storage-progress').textContent = e.storagesize;
+	})
+  //events while saving a tile layer
+  var progress;
+  baseLayer.on('savestart', function(e) {
+  	progress = 0;
+  	document.getElementById("storage-progress").textContent = e.lengthSaved + '/' + e.lengthToBeSaved;
+  });
+  baseLayer.on('savetileend', function(e) {
+  	progress++;
+  	document.getElementById("storage-progress").textContent = e.lengthSaved + '/' + e.lengthToBeSaved;
+  });
+  baseLayer.on('loadend', function(e) {
+    //alert("Saved all tiles");
+    previousStorageSize = e.storagesize
+    document.getElementById('storage-progress').textContent = e.storagesize;
+    document.getElementById('save-control').disabled = false;
+    document.getElementById('save-control').className = "fa fa-download";
+  });
+  baseLayer.on('tilesremoved', function(e) {
+    //alert("Removed all tiles");
+    previousStorageSize = e.storagesize
+    document.getElementById('storage-progress').textContent = e.storagesize;
+    document.getElementById('save-control').disabled = false;
+    document.getElementById('save-control').className = "fa fa-download";
+  });
+  return control;
+}
